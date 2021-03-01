99655d8473a9d8f463d64e65b090f31573d1cf65
Deshaine <Deshaine@gmail.com>
2021-03-01 14:34:44 +0000

    Add add_task template

--- app.py
+++ app.py
@@ -91,12 +91,17 @@ def profile(username):
     return redirect(url_for("login"))
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-      # remove user from session cookies
-      flash("You've been logged out")
-      session.pop("user")
-      return redirect(url_for("login"))
+    # remove user from session cookies
+    flash("You've been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+
+@app.route("/add_task")
+def add_task():
+    return render_template("add_task.html")
 
 
 if __name__ == "__main__":
